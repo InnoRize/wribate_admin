6a9031c036c88f22ea22fc1de531fc4aedd41518
saikrishna488 <oltvez@gmail.com>
2025-05-28 11:46:20 +0530

    28/5/25

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // distDir: './dist', 
-    eslint: {
-    // This disables ESLint during both `next build` and `next dev`
-    ignoreDuringBuilds: true,
-  },
+
   }
    
   export default nextConfig
